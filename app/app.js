0a20b675a9b035259188f93ec2e147d2a093b41f
PeterBFoo <pborrasexposito@cifpfbmoll.eu>
2022-12-31 14:12:34 +0100

    feat: configure app to log connection with database

--- app/app.js
+++ app/app.js
@@ -1,13 +1,26 @@
 const createError = require("http-errors");
 const express = require("express");
 const logger = require("morgan");
+const env = require("./src/config/dbconfig");
 const app = express();
+const db = require("./src/db/connection.js");
 
-app.use(logger("dev"));
+db.connection.sync()
+  .then(() => {
+    console.log("\x1b[36m%s\x1b[0m", "\nSynced database");
+  })
+  .catch((err) => {
+    console.log("\x1b[31m%s\x1b[0m", "Failed to sync database due to: " + err.message);
+  });
+
+if (env.NODE_ENV === "dev") {
+  app.use(logger("dev"));
+}
+
 app.use(express.json());
 app.use("/", require("./src/routes/index"));
 app.use("/bycicles", require("./src/routes/Bycicles"));
-
+app.use("/stores", require("./src/routes/Stores"));
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -21,6 +34,6 @@ app.use(function (err, req, res, next) {
   res.locals.error = env.NODE_ENV === "dev" ? err : {};
 });
 
-console.log("App is running on http://localhost:3000");
+console.log("App is running on http://localhost:" + env.PORT + "\n");
 
 module.exports = app;
