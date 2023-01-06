08dd0e9b482218daf31ef2289de65e38c9491395
PeterBFoo <pborrasexposito@cifpfbmoll.eu>
2023-01-06 22:37:36 +0100

    feat: add port to db connection

--- app/src/db/connection.js
+++ app/src/db/connection.js
@@ -9,7 +9,7 @@ const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
 	host: dbConfig.HOST,
 	dialect: dbConfig.DIALECT,
 	logging: dbConfig.LOGGING,
-
+	port: dbConfig.PORT,
 	pool: {
 		max: dbConfig.pool.max,
 		min: dbConfig.pool.min,
