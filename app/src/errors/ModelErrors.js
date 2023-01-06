8e0fd696ae0848b7f9cdf713f8ee9c63dafc2d1f
PeterBFoo <pborrasexposito@cifpfbmoll.eu>
2023-01-06 22:38:03 +0100

    feat: create custom errors for models

--- app/src/errors/ModelErrors.js
+++ app/src/errors/ModelErrors.js
@@ -1,25 +1,32 @@
 function ModelError(model) {
-    this.model = model;
+	this.model = model.toUpperCase();
 }
 
-ModelError.prototype.propertyType = function (wrongType, desiredType) {
-    throw this.model.toUpperCase() + ": " + wrongType + " must be of type " + desiredType;
-}
+ModelError.prototype.propertyType = function (propertyName, wrongType, desiredType) {
+	throw this.model + ": " + propertyName + ": " + wrongType + " must be of type " + desiredType;
+};
 
 ModelError.prototype.notNullable = function (property) {
-    throw this.model.toUpperCase() + ": " + property + " cannot be null";
-}
+	throw this.model + ": " + property + " cannot be null";
+};
+
+ModelError.prototype.invalidProperties = function () {
+	throw this.model + ": Invalid properties";
+};
 
 var modelError = function (model) {
-    let error = new ModelError(model);
-    return {
-        propertyType: function (wrongType, desiredType) {
-            return error.propertyType(wrongType, desiredType);
-        },
-        notNullable: function (property) {
-            return error.notNullable(property).call(error);
-        }
-    }
+	let error = new ModelError(model);
+	return {
+		propertyType: function (propertyName, wrongType, desiredType) {
+			return error.propertyType(propertyName, wrongType, desiredType);
+		},
+		notNullable: function (property) {
+			return error.notNullable(property).call(error);
+		},
+		invalidProperties: function () {
+			return error.invalidProperties();
+		}
+	};
 };
 
 module.exports = modelError;
