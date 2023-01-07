bbf50fd4601bbdcafdaad48461db567e56649a49
PeterBFoo <pborrasexposito@cifpfbmoll.eu>
2023-01-07 19:51:58 +0100

    feat: add coverage and test sequencer

---
+++ app/test/testSequencer.js
@@ -0,0 +1,11 @@
+// testSequencer.js
+const Sequencer = require("@jest/test-sequencer").default;
+
+class CustomSequencer extends Sequencer {
+	sort(tests) {
+		const copyTests = Array.from(tests);
+		return copyTests.sort((testA, testB) => (testA.path > testB.path ? 1 : -1));
+	}
+}
+
+module.exports = CustomSequencer;
