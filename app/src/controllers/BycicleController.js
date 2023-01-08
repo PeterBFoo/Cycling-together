46f9cf804f2bcb509a06a5f47b73ba2241066ba0
PeterBFoo <pborrasexposito@cifpfbmoll.eu>
2023-01-08 21:25:17 +0100

    refactor: use module pattern on controllers

--- app/src/controllers/BycicleController.js
+++ app/src/controllers/BycicleController.js
@@ -1,132 +1,181 @@
 const Bycicle = require("../db/connection.js").bycicles;
 
-
-// Create and Save a new Bycicle
-function registerBycicle(req, res) {
-	// Save Bycicle in the database
-	Bycicle.create(req.body)
-		.then(data => {
-			res.status(201).send({
-				message: "Bycicle was registered successfully.",
-				data: data
-			});
-		})
-		.catch(err => {
-			res.status(500).send({
-				message:
-					err.message || "Some error occurred while creating the Bycicle."
-			});
-		});
-}
-
-function findAllBycicles(req, res) {
-	Bycicle.findAll({ where: {} })
-		.then(data => {
-			res.status(200).send(data);
-		})
-		.catch(err => {
-			res.status(500).send({
-				message:
-					err.message || "Some error occurred while retrieving Bycicles."
-			});
-		});
-}
-
-// Find a single Bycicle with an id
-function findOneBycicle(req, res) {
-	let id = req.params.id;
-
-	Bycicle.findByPk(id)
-		.then(data => {
-			if (data) {
-				res.send(data);
-			} else {
-				res.status(404).send({
-					message: `Cannot find Bycicle with id=${id}.`
+var controller = (function () {
+	// Create and Save a new Bycicle
+	function registerBycicle(req, res) {
+		// Save Bycicle in the database
+		Bycicle.create(req.body)
+			.then(data => {
+				res.status(201).send({
+					message: "Bycicle was registered successfully.",
+					data: data
 				});
-			}
-		})
-		.catch(err => {
-			res.status(500).send({
-				message: `Error retrieving Bycicle with id=${id}. ${err}`
+			})
+			.catch(err => {
+				if (err.message.includes("bycicles_storeId_fkey")) {
+					res.status(400).send({
+						message: `Store with id ${req.body.storeId} not found.`
+					});
+				} else {
+					res.status(500).send({
+						message:
+							err.message || "Some error occurred while creating the Bycicle."
+					});
+				}
 			});
-		});
-}
+	}
 
-// Update a Bycicle by the id in the request
-function updateBycicle(req, res) {
-	let id = req.params.id;
-
-	Bycicle.update(req.body, {
-		where: { id: id }
-	})
-		.then(num => {
-			if (num == 1) {
-				res.status(200).send({
-					message: "Bycicle was updated successfully."
+	function filterByCategory(req, res) {
+		let category = req.params.category;
+		Bycicle.findAll({ where: { category: category } })
+			.then(data => {
+				if (data.length == 0) {
+					res.status(404).send({
+						message: `No Bycicles found with category ${category}.`
+					});
+				} else {
+					res.status(200).send(data);
+				}
+			})
+			.catch(err => {
+				res.status(500).send({
+					message:
+						err.message || "Some error occurred while retrieving Bycicles."
 				});
-			} else {
-				res.status(404).send({
-					message: `Cannot update Bycicle with id=${id}.`
-				});
-			}
-		})
-		.catch(err => {
-			res.status(500).send({
-				message: `Error updating Bycicle with id=${id}. ${err}`
 			});
-		});
-}
+	}
 
-// Delete a Bycicle with the specified id in the request
-function deleteBycicle(req, res) {
-	let id = req.params.id;
-
-	Bycicle.destroy({
-		where: { id: id }
-	})
-		.then(num => {
-			if (num == 1) {
-				res.status(204).send({
-					message: "Bycicle was deleted successfully!"
+	function filterByBrand(req, res) {
+		let brand = req.params.brand;
+		Bycicle.findAll({ where: { brand: brand } })
+			.then(data => {
+				if (data.length == 0) {
+					res.status(404).send({
+						message: `No Bycicles found with brand ${brand}.`
+					});
+				} else {
+					res.status(200).send(data);
+				}
+			})
+			.catch(err => {
+				res.status(500).send({
+					message:
+						err.message || "Some error occurred while retrieving Bycicles."
 				});
-			} else {
-				res.status(404).send({
-					message: `Cannot delete Bycicle with id=${id}. Maybe Bycicle was not found!`
+			});
+	}
+
+	function findAllBycicles(req, res) {
+		Bycicle.findAll({ where: {} })
+			.then(data => {
+				res.status(200).send(data);
+			})
+			.catch(err => {
+				res.status(500).send({
+					message:
+						err.message || "Some error occurred while retrieving Bycicles."
 				});
-			}
-		})
-		.catch(err => {
-			res.status(500).send({
-				message: "Could not delete Bycicle with id=" + id, err
 			});
-		});
-}
+	}
 
-// Delete all Bycicles from the database.
-function deleteAllBycicles(req, res) {
-	Bycicle.destroy({
-		where: {},
-		truncate: false
-	})
-		.then(nums => {
-			res.send({ message: `${nums} Bycicles were deleted successfully!` });
-		})
-		.catch(err => {
-			res.status(500).send({
-				message:
-					err.message || "Some error occurred while removing all Bycicles."
+	// Find a single Bycicle with an id
+	function findOneBycicle(req, res) {
+		let id = req.params.id;
+
+		Bycicle.findByPk(id)
+			.then(data => {
+				if (data) {
+					res.send(data);
+				} else {
+					res.status(404).send({
+						message: `Cannot find Bycicle with id=${id}. Bycicle was not found.`
+					});
+				}
+			})
+			.catch(err => {
+				res.status(500).send({
+					message: `Error retrieving Bycicle with id=${id}. ${err}`
+				});
 			});
-		});
-}
+	}
 
-var bycicleController = {
-	registerBycicle,
-	findAllBycicles,
-	findOneBycicle,
-	updateBycicle,
-	deleteBycicle,
-	deleteAllBycicles
-};
+	// Update a Bycicle by the id in the request
+	function updateBycicle(req, res) {
+		let id = req.params.id;
 
-module.exports = bycicleController;
+		Bycicle.update(req.body, {
+			where: { id: id }
+		})
+			.then(num => {
+				if (num == 1) {
+					res.status(200).send({
+						message: "Bycicle was updated successfully."
+					});
+				} else {
+					res.status(404).send({
+						message: `Cannot update Bycicle with id=${id}. Bycicle was not found.`
+					});
+				}
+			})
+			.catch(err => {
+				res.status(500).send({
+					message: `Error updating Bycicle with id=${id}. ${err}`
+				});
+			});
+	}
+
+	// Delete a Bycicle with the specified id in the request
+	function deleteBycicle(req, res) {
+		let id = req.params.id;
+
+		Bycicle.destroy({
+			where: { id: id }
+		})
+			.then(num => {
+				if (num == 1) {
+					res.status(204).send({
+						message: "Bycicle was deleted successfully!"
+					});
+				} else {
+					res.status(404).send({
+						message: `Cannot delete Bycicle with id=${id}. Maybe Bycicle was not found!`
+					});
+				}
+			})
+			.catch(err => {
+				res.status(500).send({
+					message: "Could not delete Bycicle with id=" + id, err
+				});
+			});
+	}
+
+	// Delete all Bycicles from the database.
+	function deleteAllBycicles(req, res) {
+		Bycicle.destroy({
+			where: {},
+			truncate: false
+		})
+			.then(nums => {
+				res.send({ message: `${nums} Bycicles were deleted successfully!` });
+			})
+			.catch(err => {
+				res.status(500).send({
+					message:
+						err.message || "Some error occurred while removing all Bycicles."
+				});
+			});
+	}
+
+	return {
+		registerBycicle,
+		filterByCategory,
+		filterByBrand,
+		findAllBycicles,
+		findOneBycicle,
+		updateBycicle,
+		deleteBycicle,
+		deleteAllBycicles
+	};
+})();
+
+module.exports = controller;
--- app/src/controllers/StoreController.js
+++ app/src/controllers/StoreController.js
@@ -1,146 +1,148 @@
 const Store = require("../db/connection.js").stores;
 
+var controller = (function () {
 
-function findAll(req, res) {
-	Store.findAll({ where: {} })
-		.then(data => {
-			res.status(200).send(data);
-		})
-		.catch(err => {
-			res.status(500).send({
-				message:
-					err.message || "Some error occurred while retrieving Stores."
-			});
-		});
-}
-
-function findAssociatedBikes(req, res) {
-	const storeId = req.params.id;
-	Store.findByPk(storeId, { include: ["bycicles"] })
-		.then(data => {
-			res.status(200).send(data);
-		})
-		.catch(err => {
-			res.status(500).send({
-				message: "Error retrieving Store with id=" + storeId, err
-			});
-		});
-}
-
-function findOne(req, res) {
-	const id = req.params.id;
-
-	Store.findByPk(id)
-		.then(data => {
-			if (data) {
+	function findAllStores(req, res) {
+		Store.findAll({ where: {} })
+			.then(data => {
 				res.status(200).send(data);
-			} else {
-				res.status(404).send({
-					message: `Cannot find Store with id=${id}.`
+			})
+			.catch(err => {
+				res.status(500).send({
+					message:
+						err.message || "Some error occurred while retrieving Stores."
 				});
-			}
+			});
+	}
+
+	function findAssociatedBikes(req, res) {
+		const storeId = req.params.id;
+		Store.findByPk(storeId, { include: ["bycicles"] })
+			.then(data => {
+				res.status(200).send(data);
+			})
+			.catch(err => {
+				res.status(500).send({
+					message: "Error retrieving Store with id=" + storeId, err
+				});
+			});
+	}
+
+	function findOneStore(req, res) {
+		const id = req.params.id;
+
+		Store.findByPk(id)
+			.then(data => {
+				if (data) {
+					res.status(200).send(data);
+				} else {
+					res.status(404).send({
+						message: `Cannot find Store with id=${id}. Store not found.`
+					});
+				}
+			})
+			.catch(err => {
+				res.status(500).send({
+					message: "Error retrieving Store with id=" + id, err
+				});
+			});
+	}
+
+	function updateStore(req, res) {
+		const id = req.params.id;
+
+		Store.update(req.body, {
+			where: { id: id }
 		})
-		.catch(err => {
-			res.status(500).send({
-				message: "Error retrieving Store with id=" + id, err
+			.then(num => {
+				if (num == 1) {
+					res.status(200).send({
+						message: "Store was updated successfully."
+					});
+				} else {
+					res.status(404).send({
+						message: `Cannot update Store with id=${id}. Store not found.`
+					});
+				}
+			})
+			.catch(err => {
+				res.status(500).send({
+					message: "Error updating Store with id=" + id, err
+				});
 			});
-		});
-}
+	}
 
-function update(req, res) {
-	const id = req.params.id;
+	function registerStore(req, res) {
+		Store.create(req.body)
+			.then(data => {
+				res.status(201).send({
+					message: "Store was registered successfully.",
+					data: data
+				});
+			})
+			.catch(err => {
+				res.status(500).send({
+					message:
+						err.message || "Some error occurred while creating the Store."
+				});
+			});
+	}
 
-	Store.update(req.body, {
-		where: { id: id }
-	})
-		.then(num => {
-			if (num == 1) {
-				res.status(200).send({
-					message: "Store was updated successfully."
-				});
-			} else {
-				res.status(404).send({
-					message: `Cannot update Store with id=${id}.`
-				});
-			}
+	function deleteOne(req, res) {
+		const id = req.params.id;
+
+		Store.destroy({
+			where: { id: id }
 		})
-		.catch(err => {
-			res.status(500).send({
-				message: "Error updating Store with id=" + id, err
-			});
-		});
-}
-
-function create(req, res) {
-	Store.create(req.body)
-		.then(data => {
-			res.status(201).send({
-				message: "Store was registered successfully.",
-				data: data
-			});
-		})
-		.catch(err => {
-			res.status(500).send({
-				message:
-					err.message || "Some error occurred while creating the Store."
-			});
-		});
-}
-
-function deleteOne(req, res) {
-	const id = req.params.id;
-
-	Store.destroy({
-		where: { id: id }
-	})
-		.then(num => {
-			if (num == 1) {
-				res.status(204).send({
-					message: "Store was deleted successfully!"
+			.then(num => {
+				if (num == 1) {
+					res.status(204).send({
+						message: "Store was deleted successfully!"
+					});
+				} else {
+					res.status(404).send({
+						message: `Cannot delete Store with id=${id}. Store not found. `
+					});
+				}
+			})
+			.catch(err => {
+				res.status(500).send({
+					message: "Could not delete Store with id=" + id, err
 				});
-			} else {
-				res.status(404).send({
-					message: `Cannot delete Store with id=${id}. Store not found. `
-				});
-			}
-		})
-		.catch(err => {
-			res.status(500).send({
-				message: "Could not delete Store with id=" + id, err
 			});
-		});
-}
+	}
 
-function deleteAll(req, res) {
-	Store.destroy({
-		where: {},
-		truncate: false
-	})
-		.then(nums => {
-			if (nums == 0) {
-				res.status(304).send({
-					message: "Cannot delete stores because there are no stores in the database."
-				});
-			} else {
-				res.status(204).send({ message: `${nums} Stores were deleted successfully!` });
-			}
+	function deleteAll(req, res) {
+		Store.destroy({
+			where: {},
+			truncate: false
 		})
-		.catch(err => {
-			res.status(500).send({
-				message:
-					err.message || "Some error occurred while removing all Stores."
+			.then(nums => {
+				if (nums == 0) {
+					res.status(304).send({
+						message: "Cannot delete stores because there are no stores in the database."
+					});
+				} else {
+					res.status(204).send({ message: `${nums} Stores were deleted successfully!` });
+				}
+			})
+			.catch(err => {
+				res.status(500).send({
+					message:
+						err.message || "Some error occurred while removing all Stores."
+				});
 			});
-		});
-}
+	}
 
-var storeController = {
-	findAll,
-	findOne,
-	update,
-	create,
-	deleteOne,
-	deleteAll,
-	findAssociatedBikes
-};
+	return {
+		findAllStores,
+		findOneStore,
+		updateStore,
+		registerStore,
+		deleteOne,
+		deleteAll,
+		findAssociatedBikes
+	};
+})();
 
-module.exports = storeController;
+module.exports = controller;
