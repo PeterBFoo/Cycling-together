1974aa6a1f8abfc6062ba2dbacba56b1495080ba
PeterBFoo <pborrasexposito@cifpfbmoll.eu>
2023-01-08 21:30:42 +0100

    feat: add production database on init.sql

--- docker/init.sql
+++ docker/init.sql
@@ -1,5 +1,7 @@
+DROP DATABASE IF EXISTS cycling_together;
 DROP DATABASE IF EXISTS cycling_together_dev;
 DROP DATABASE IF EXISTS cycling_together_test;
 
+CREATE DATABASE cycling_together;
 CREATE DATABASE cycling_together_dev;
 CREATE DATABASE cycling_together_test;
