3cde57d7519ba1790d75b8293a432b2da4bfebbd
PeterBFoo <pborrasexposito@cifpfbmoll.eu>
2023-01-09 11:04:12 +0100

    refactor: models scheme

--- app/src/models/Models.js
+++ app/src/models/Models.js
@@ -1,14 +1,14 @@
 const DataTypes = require("sequelize");
-const bycicleModel = require("../models/bycicle/Bycicle.js");
-const storeModel = require("../models/store/Store.js");
+const bycicleModel = require("../models/bycicle/Bycicle.js").get();
+const storeModel = require("../models/store/Store.js").get();
 
 const Models = {
 	bycicle: function (sequelize) {
-		const Bycicle = sequelize.define("bycicles", bycicleModel.get().properties(DataTypes));
+		const Bycicle = sequelize.define("bycicles", bycicleModel.properties(DataTypes));
 		return Bycicle;
 	},
 	store: function (sequelize) {
-		const Store = sequelize.define("stores", storeModel.get().properties(DataTypes));
+		const Store = sequelize.define("stores", storeModel.properties(DataTypes));
 		return Store;
 	}
 };
