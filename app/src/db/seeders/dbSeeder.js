bf6e66289414351cf29d42c95752e0d864e0bf33
PeterBFoo <pborrasexposito@cifpfbmoll.eu>
2023-01-15 13:09:01 +0100

    feat: add booking seeder

--- app/src/db/seeders/dbSeeder.js
+++ app/src/db/seeders/dbSeeder.js
@@ -1,14 +1,17 @@
 const bycicleSeeder = require("./bycicle/BycicleSeeder");
 const storeSeeder = require("./store/StoreSeeder");
+const bookingSeeder = require("./booking/BookingSeeder");
 
 var dbActions = {
 	seed: async () => {
 		await storeSeeder.up();
 		await bycicleSeeder.up();
+		await bookingSeeder.up();
 	},
 	drop: async () => {
 		await storeSeeder.down();
 		await bycicleSeeder.down();
+		await bookingSeeder.down();
 	}
 };
 
